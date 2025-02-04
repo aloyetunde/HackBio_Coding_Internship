528dde740602a1095a9d47d2feb2dbf802844675
aloyetunde <107317858+aloyetunde@users.noreply.github.com>
2025-02-04 20:00:59 +0100

    Update code.py

--- STAGE_0/code.py
+++ STAGE_0/code.py
@@ -1,1 +1,45 @@
+# Person 1
+name1 = "Alo Yetunde"
+slack1 = "MaryAde364"
+email1 = "aloyetunde99@gmail.com"
+hobby1 = "Reading"
+country1 = "Nigeria"
+discipline1 = "Biochemistry"
+language1 = "Python"
+
+# Person 2
+name2 = "Oluchi Bolanle"
+slack2 = "Oluchi Bolanle"
+email2 = "oluchibolanle@gmail.com"
+hobby2 = ""
+country2 = ""
+discipline2 = ""
+language2 = "Python"
+
+# Person 3
+name3 = "Adewumi Ifeoluwa"
+slack3 = "Ifeoluwa"
+email3 = "iadewunmi39@gmail.com"
+hobby3 = ""
+country3 = ""
+discipline3 = ""
+language3 = "Python"
+
+# Person 4
+name4 = "Adedayo adeboye"
+slack4 = "Adedayo"
+email4 = "timsonad09@gmail.com"
+hobby4 = ""
+country4 = ""
+discipline4 = ""
+language4 = "Python"
+
+# Person 5
+name5 = "Omojuwa Iyinoluwa"
+slack5 = "Iyinomj"
+email5 = "iyinomojuwa1@gmail.com"
+hobby5 = ""
+country5 = ""
+discipline5 = ""
+language5 = "Python"
 
